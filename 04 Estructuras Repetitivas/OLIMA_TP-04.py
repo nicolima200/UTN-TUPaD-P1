5f275cd4d614f122f4e41f18855734f6d3f1bda6
nicolima200 <nicolima200@gmail.com>
2025-04-07 18:57:42 -0300

    Comentario agregado al TP4

--- 04 Estructuras Repetitivas/OLIMA_TP-04.py
+++ 04 Estructuras Repetitivas/OLIMA_TP-04.py
@@ -1,3 +1,9 @@
+### ATENCIÓN!!!
+### Al final del documento 
+### se encuentran las llamadas
+### a las funciones
+
+
 # # # 1) Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100
 # # # (incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
 
